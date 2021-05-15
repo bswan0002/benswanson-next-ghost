23fbcb775243daa5404bc5c424e34706caed9da7
bswan0002 <bswan0002@gmail.com>
2021-05-15 11:13:23 -0700

    font

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,5 @@
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
@@ -7,20 +8,20 @@ export default function Footer() {
       <div>
         <div className="py-10 flex flex-col items-center">
           <h3 className="text-3xl mb-8">Don't be a stranger ;)</h3>
-          <div className="text-lg md:text-xl inline-flex flex-row flex-wrap justify-center place-items-center space-x-7 md:space-x-8">
-            <a href="mailto:bswan0002@gmail.com" className="link-classic">
-              bswan0002@gmail.com
+          <div className="text-lg md:text-xl inline-flex flex-row flex-wrap justify-center place-items-center space-x-10">
+            <a href="mailto:bswan0002@gmail.com" className="link-classic w-5">
+              <FontAwesomeIcon icon={faEnvelope} />
             </a>
 
             <a
               href="https://github.com/bswan0002/"
-              className="link-classic text-gray-300 h-5 w-5"
+              className="link-classic text-gray-300 w-5"
             >
               <FontAwesomeIcon icon={faGithub} />
             </a>
             <a
               href="https://www.linkedin.com/in/bswan0002/"
-              className="link-classic h-5 w-5"
+              className="link-classic w-5"
             >
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
