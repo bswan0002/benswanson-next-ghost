e92ac9f565b7a611ddf4c7a9f2b4aeb13c03f69a
bswan0002 <bswan0002@gmail.com>
2021-06-22 12:29:58 -0700

    what is happening

--- pages/posts.js
+++ pages/posts.js
@@ -11,7 +11,7 @@ export default function Posts({ posts }) {
     <Layout>
       <div className="px-2 mt-8 md:px-0">
         <h1>&#9997;&#65039; Posts</h1>
-        <h2 className="mt-8">this is an h2</h2>
+        <h2 className="mt-8">this is an h2 fix?</h2>
         {renderPosts()}
       </div>
     </Layout>
